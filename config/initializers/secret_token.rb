f88a97c2380abcb2cc3363979a281bdad949b7a9
gal <galharth960@gmail.com>
2011-03-23 23:02:50 +0200

    2 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoiceApp::Application.config.secret_token = 'd0dc27a56035ac6df4193ea57e14af0fa42e931fab3bc4d911143a1f63b8cfde56c189ed82f888ee25df7c8002f2c453ae33951b1584a2fc5b0afe6635878a01'
